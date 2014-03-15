53d7226886c1a953e1e707a55c13d687ad77808f
German <german.lena@gmail.com>
2014-03-15 01:03:42 -0300

    avances

---
+++ assets/js/normal/all.js
@@ -0,0 +1,74 @@
+var height = $(window).height();
+$('#map').height(height);
+
+var map = L.map('map').setView([50.10714500,8.66378900], 2);
+
+/*
+L.tileLayer('http://{s}.tile.cloudmade.com/2262e8a159bb4e98bec341f62716c75c/54912/256/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
+}).addTo(map);
+*/
+
+L.tileLayer('http://{s}.tile.cloudmade.com/2262e8a159bb4e98bec341f62716c75c/125104/256/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
+}).addTo(map);
+
+
+$(document).ready(function(){
+	$.ajax('/assets/data/data.json',{
+		dataType:'json'
+	}).done(function( data ) {
+		readCountries(data,0);
+	});
+});
+
+
+
+function readCountries(data, index)
+{
+	console.log('load country ' + index);
+	if (index == data.length) return;
+
+	var country = data[index];
+
+	readCities(country.cities,0, data, index)
+		
+	index++;
+	/*
+	readCountries(data, index);
+	*/
+}
+
+function readCities(data,index,country_data,country_index)
+{
+	console.log('load city ' + index);
+
+	if (index == data.length) {
+		readCountries(country_data, country_index+1);
+		return;
+	}
+
+	var city = data[index];
+
+	for (key in city.points) {
+		loadPoints(city.points[key]);
+	}
+
+	index++;
+	
+	setTimeout(function(){
+		readCities(data, index, country_data, country_index);
+	},500);
+}
+
+function loadPoints(data)
+{
+	data.forEach(function(point){
+
+		L.marker([point.latitude,point.longitude]).addTo(map)
+			.bindPopup("<b>"+point.name+"</b><br /><p>"+point.description+"</p>");
+
+	});
+}
---
+++ gen/generate.php
@@ -0,0 +1,56 @@
+<?php
+
+require 'model/CountryVisit.php';
+require 'model/CityVisit.php';
+require 'model/PointVisit.php';
+require 'model/City.php';
+require 'model/Country.php';
+
+Country::Load(
+	json_decode(file_get_contents('originaldata/countries.json'))
+);
+
+City::Load(
+	json_decode(file_get_contents('originaldata/cities.json'))
+);
+
+$data = json_decode(file_get_contents('originaldata/points.json'));
+
+$newdata = array();
+
+$lastCountry = null;
+$lastCity = null;
+$obj = null;
+
+foreach ($data as $item)
+{
+    if ($lastCountry != $item->country_id)
+    {
+    	$country = Country::Get($item->country_id);
+		$countryVisit = new CountryVisit($item->country_id, $country->name);
+		$newdata[] = $countryVisit;
+        $lastCountry = $item->country_id;
+    }
+
+    if ($lastCity != $item->city_id)
+    {
+    	$city = City::Get($item->city_id);
+
+    	if ($city === null)
+    	{
+    		var_dump($item);exit;
+    	}
+
+		$cityVisit = new CityVisit($item->city_id, $city->name);
+		$countryVisit->addCity($cityVisit);
+        $lastCity = $item->city_id;
+    }
+
+	$pointVisit = new PointVisit($item);
+	$cityVisit->addPoint($pointVisit);
+
+}
+
+file_put_contents('generateddata/data.json', json_encode($newdata));
+
+?>
